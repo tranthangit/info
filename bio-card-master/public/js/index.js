138e544dbfdb05540eb99b0fbf083b6a2afba40d
tranthangit <108046122+tranthangit@users.noreply.github.com>
2022-06-27 13:46:58 +0000

    Delete bio-card-master directory

--- bio-card-master/public/js/index.js
+++
@@ -1,9 +0,0 @@
-const user = USER;
-const webConfig = WEB;
-SetTitleAndIcon(webConfig.title, webConfig.favicon);
-RenderProfileHeader(user.username, user.avatar, user.skills, user.blueTick);
-RenderBanks(user.banks);
-ShowModalEvent();
-CloseModalEvent();
-CopyEvent();
-MouseLeaveEvent();
