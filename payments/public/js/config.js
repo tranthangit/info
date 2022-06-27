47c7bc5b2a179d9c9ca3b60860d0f0a161dd83e1
tranthangit <108046122+tranthangit@users.noreply.github.com>
2022-06-27 13:47:44 +0000

    Add files via upload

---
+++ payments/public/js/config.js
@@ -0,0 +1,29 @@
+const WEB = {
+    title: "Source code by yzarc204",
+    favicon: "tat.png",
+};
+
+const USER = {
+    username: "Hà Kỳ Anh",
+    blueTick: true,
+    avatar: "tat.png",
+    skills: [
+        "Developer",
+        "Addict"
+    ],
+    banks: [
+        {
+            name: "Momo",
+            logo: "momo.png",
+            number: "0964018360",
+            owner: "Hà Kỳ Anh",
+            fastTransfer: true
+        },
+        {
+            name: "MB Bank",
+            logo: "mbbank.png",
+            number: "0964018360",
+            owner: "Hà Kỳ Anh"
+        },
+    ]
+};
